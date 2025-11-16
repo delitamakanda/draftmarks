eb082e5927c0de929dd116de1cc74f8a8c5f7bd6
Délita Makanda <delita.makanda@gmail.com>
2025-11-16 02:59:53 +0100

    add @astrojs/vercel dependency: required for Vercel integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,15 @@ import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'astro/config';
 
+import vercel from '@astrojs/vercel';
+
 // https://astro.build/config
 export default defineConfig({
-        integrations: [react()],
-        vite: {
-                plugins: [tailwindcss()],
-        },
+  integrations: [react()],
+
+  vite: {
+          plugins: [tailwindcss()],
+  },
+
+  adapter: vercel(),
 });
